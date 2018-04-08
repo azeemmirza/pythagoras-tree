70a7ed739714584732855955444e85ddb033532b
Azeem Mirza <contact@azeemirza.com>
2018-04-08 15:54:48 +0500

    canvas #3 - methods completed

--- src/headers/Canvas.h
+++ src/headers/Canvas.h
@@ -86,3 +86,83 @@ Canvas::Canvas(int width, int height, char* windowTitle)// constructor
 	*/
 	CP.set(0.0f, 0.0f, 0.0f);
 }
+
+/**
+* Set the world window
+*/
+void Canvas::setWindow(float l, float r, float b, float t)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	gluOrtho2D(l, r, b, t);
+}
+/**
+* Set the Viewport Map
+*/
+void Canvas::setViewport(int l, int r, int b, int t)
+{
+	glViewport(l, b, r - l, t - b);
+}
+void Canvas::clearScreen()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+/**
+* Set the canvas background
+*/
+void Canvas::setBackgroundColor(float r, float g, float b)
+{
+	glClearColor(r, g, b, 1.0);
+}
+/**
+* Sets color of next object
+*/
+void Canvas::setColor(float r, float g, float b)
+{
+	glColor3f(r, g, b);
+}
+/**
+* Draw line to given points from CP(Current Point)
+*/
+void Canvas::lineTo(float x, float y, float z)
+{
+	glBegin(GL_LINES);
+	glVertex2f(CP.getX(), CP.getY());
+	glVertex2f(x, y);
+	glEnd();
+
+	CP.set(x, y, z);
+}
+/**
+* Draw line to given points from CP(Current Point)
+*/
+void Canvas::lineTo(Point3 p)
+{
+	glBegin(GL_LINES);
+	glVertex2f(CP.getX(), CP.getY());
+	glVertex2f(p.getX(), p.getY());
+	glEnd();
+
+	CP.set(p.getX(), p.getY(), p.getZ());
+}
+/**
+* Change CP(Current Point)
+*/
+void Canvas::moveTo(float x, float y, float z)
+{
+	CP.set(x, y, z);
+}
+/**
+* Change CP(Current Point)
+*/
+void Canvas::moveTo(Point3 p)
+{
+	CP.set(p.getX(), p.getY(), p.getZ());
+}
+/**
+* returns CP(Current Point) on canvas
+*/
+Point3 Canvas::getCP() {
+	return CP;
+}
