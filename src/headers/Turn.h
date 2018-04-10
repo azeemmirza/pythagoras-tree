8a56417efcc7c3f8a44803daa0bc75574b236635
Azeem Mirza <contact@azeemirza.com>
2018-04-10 16:23:09 +0500

    Turn enum constructed

---
+++ src/headers/Turn.h
@@ -0,0 +1,23 @@
+/**
+* @file Turn.h
+*
+* @author azeemmirza
+* @version 1.0.0
+* @date 10/04/18
+*/
+#pragma once
+#ifndef TURN_H
+
+/**
+* @class Turn
+* @description Enum class for TURN selection
+*/
+
+enum Turn {
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+};
+
+#endif // !TURN_H
