b1151b83ebc7df6c5066dd6cc12f80b0f80a6774
Azeem Mirza <contact@azeemirza.com>
2018-06-20 22:01:10 +0500

    Square class Docs updated

--- src/headers/Square.h
+++ src/headers/Square.h
@@ -1,3 +1,10 @@
+/**
+* @file Square.h
+*
+* @author azeemmirza
+* @version 1.0
+* @date 20/06/18
+*/
 #pragma once
 #include "Point.h"
 
@@ -14,10 +21,16 @@ public:
 };
 
 Square::Square(Point3 i, float s) {
-	float x = i.getX(), y = i.getY(), z = i.getZ();
-	float ss = s / 2;
-	float xx = x - ss, yy = y - ss, zz = z - ss;
-	float xxx = x + ss, yyy = y + ss, zzz = z + ss;
+	float x = i.getX(), 
+		y = i.getY(), 
+		z = i.getZ();
+	float ss = s / 2,
+		xx = x - ss, 
+		yy = y - ss, 
+		zz = z - ss;
+	float xxx = x + ss, 
+		yyy = y + ss, 
+		zzz = z + ss;
 
 	glBegin(GL_POLYGON);
 
