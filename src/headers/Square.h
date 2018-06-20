b4a2db591ad2039ed1fb82b203879d6134c405d2
Azeem Mirza <contact@azeemirza.com>
2018-06-20 21:59:54 +0500

    Sqaure class

---
+++ src/headers/Square.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "Point.h"
+
+#ifndef SQUARE_H
+
+/**
+ * @class Square
+ * @description Sqaure class to draw square with w.r.t center point.
+*/
+
+class Square {
+public:
+	Square(Point3 i, float s);
+};
+
+Square::Square(Point3 i, float s) {
+	float x = i.getX(), y = i.getY(), z = i.getZ();
+	float ss = s / 2;
+	float xx = x - ss, yy = y - ss, zz = z - ss;
+	float xxx = x + ss, yyy = y + ss, zzz = z + ss;
+
+	glBegin(GL_POLYGON);
+
+	glVertex3f(xx, yy, zz);
+	glVertex3f(xxx, yy, zz);
+	glVertex3f(xxx, yyy, zz);
+
+	glVertex3f(xx, yy, zz);
+	glVertex3f(xxx, yyy, zz);
+	glVertex3f(xx, yyy, zz);
+
+	glVertex3f(xx, yy, zz); 
+	glVertex3f(xx, yyy, zz);
+	glVertex3f(xx, yyy, zzz);
+
+	glVertex3f(xx, yy, zz);
+	glVertex3f(xx, yy, zzz);
+	glVertex3f(xx, yyy, zzz);
+
+	glVertex3f(xx, yy, zzz);
+	glVertex3f(xxx, yyy, zzz);
+	glVertex3f(xx, yyy, zzz);
+
+	glVertex3f(xx, yy, zzz);
+	glVertex3f(xxx, yyy, zzz);
+	glVertex3f(xxx, yy, zzz);
+							 							 
+	glVertex3f(xxx, yy, zzz);
+	glVertex3f(xxx, yyy, zz); 
+	glVertex3f(xxx, yyy, zzz);
+							 
+	glVertex3f(xxx, yy, zzz);
+	glVertex3f(xxx, yyy, zz);
+	glVertex3f(xxx, yy, zz);
+
+	glVertex3f(xx, yyy, zz);
+	glVertex3f(xx, yyy, zzz);
+	glVertex3f(xxx, yyy, zz);
+							  
+	glVertex3f(xxx, yyy, zzz);
+	glVertex3f(xx, yyy, zzz);
+	glVertex3f(xxx, yyy, zz);
+
+	glVertex3f(xx, yy, zz);
+	glVertex3f(xx, yy, zzz);
+	glVertex3f(xxx, yy, zz);
+							 
+	glVertex3f(xxx, yy, zzz);
+	glVertex3f(xx, yy, zzz);
+	glVertex3f(xxx, yy, zz); 
+
+
+	glEnd();
+}
+
+
+#endif //SQUARE_H
