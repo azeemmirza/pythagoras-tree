ca9518306c158c7c4212f2c8ec839ac8ed0592b5
Azeem Mirza <contact@azeemirza.com>
2018-06-24 02:23:29 +0500

    Main File functions coded - unfinished

--- src/pythagoras-tree.cpp
+++ src/pythagoras-tree.cpp
@@ -1,13 +1,175 @@
 #include <string>
 #include <iostream>
 #include "copyrights.h"
+#include "headers/Canvas.h";
+#include "headers/CONFIG.h";
+
+float aaa = 60;
+void surface();
 
 
 
-int main (void) {
-	
+
+void pyth() {
+	std::string axiom = "S";
+	int iterations = 10;
+	std::map <char, std::string> ruleSet;
+	std::pair <char, std::string> R1('S', "X [ - M S ] [ + M S ]");
+	ruleSet.insert(R1);
+
+	RuleEngine r(axiom, ruleSet, iterations);
+
+	std::string s = r.getResult();
+	std::cout << s << std::endl;
+	glColor3f(1, 1, 0);
+
+	LSystem l(s, 20, aaa);
+	Point3 pp(0, 0, 0);
+	pp.setSize(10.0); pp.draw();
+	surface();
+	glColor3f(0, 1, 1);
+	l.engine();
+
+}
+
+void surface() {
+	float a = 100;
+	glColorMaterial(GL_FRONT, GL_3D);
+	glEnable(GL_COLOR_MATERIAL);
+	glColor3f(0.87, 0.72, 0.52);
+	glBegin(GL_POLYGON);
+	glVertex3f(-a, 0, a);
+	glVertex3f(-a, 0, -a);
+	glVertex3f(a, 0, -a);
+	glVertex3f(a, 0, a);
+	glEnd();
+}
+
+
+
+
+void display() {
+	/*
+	glColor3f(1, 1, 0);
+	Point3 pp(0,0,0);
+	pp.setSize(10.0);
+	pp.draw();
+
+	glPushMatrix();
+	glRotatef(45, 0,0,1);
+	Square sq(pp, 10);
+	glTranslatef(-10, 0, 0);
+	glPopMatrix();
+
+
+	glColor3f(1, 1, 1);
+	Point3 ppe(10, 10, 0);
+	ppe.setSize(10.0);
+	ppe.draw();
+	float angal = 30;
+	float size = 10;
+	float aa = calculateX(ppe,angal), bb = calculateY(ppe,angal);
+	//printf("x: %f y: %f", aa,bb);
+
+
+	glPushMatrix();
+	glTranslatef(ppe.getX(), ppe.getY(), ppe.getZ());
+	glRotatef(angal, 0, 0, 1);
+	glTranslatef(-ppe.getX(), -ppe.getY(), -ppe.getZ());
+	Square sqq(ppe, size);
+	glPopMatrix();
+
+	ppe.set(30,0,0);
+	glPushMatrix();
+	glTranslatef(ppe.getX(), ppe.getY(), ppe.getZ());
+	glRotatef(angal, 0, 0, 1);
+	glTranslatef(-ppe.getX(), -ppe.getY(), -ppe.getZ());
+	Square sqrq(ppe, size);
+	glPopMatrix();
+	*/
+	glPushMatrix();
+	glTranslatef(0, -20, -100);
+	pyth();
+	glPopMatrix();
+	glFlush();
+
+}
+
+
+
+void initLighting()
+{
+	GLfloat qaAmbientLight[] = { 0, 0, 0, 1.0 };
+	GLfloat qaDiffuseLight[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat qaSpecularLight[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat emitLight[] = { 1, 0.9, 0.9, 0.01 };
+	GLfloat Noemit[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat qaLightPosition[] = { 0, 0, -50, 1 };
+	// Enable lighting
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	// Set lighting intensity and color
+	glLightfv(GL_LIGHT0, GL_AMBIENT, qaAmbientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, qaDiffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);
+
+	// Set the light position
+	glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
+
+}
+
+
+void key(unsigned char c, int x, int y) {
+	if (c == 'a') {
+		aaa += 1;
+	}
+	else if (c == 's') {
+		aaa += 1;
+
+	}
+}
+void reshape(int w, int h) {
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//glOrtho(-200, 200, -200, 200, -200, 200);
+	//glOrtho(-30, 30, -30, 30, -30, 30);
+
+
+
+	gluPerspective(90.0, (GLdouble)w / (GLdouble)h, 0, 200.0);
+	//gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, w, h);
+}
+
+
+
+/**
+  * @function main
+  * @params Argument Count [argc], Argument Vector [argv]
+  */
+int main (int argc, char** argv) {
+
+	int cvsWinWidth = WIN_SIZE_WIDTH;
+	int cvsWinHeight = WIN_SIZE_HEIGHT;
+	std::string cvsWinTitle = WIN_TITLE;
+
+	Canvas cvs(argc, argv, 1000, 1000, "Pythgoras Tree");
+
 	std::cout << copyrights() << std::endl;
-	getchar();
+	//getchar();
+
+	//initLighting();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	//glutKeyboardFunc(key);
+	glutMainLoop();
+
 	return 0;
 
 }
