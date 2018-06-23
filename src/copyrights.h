f808c7ba4f5dddba85245a275958213cde1fdae9
Azeem Mirza <contact@azeemirza.com>
2018-06-24 01:28:10 +0500

    copyrights file updated

--- src/copyrights.h
+++ src/copyrights.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <iostream>
 #include <string.h>
+
+/*
 class copyrights {
-	std::string name = "Azeem Mirza",
-		year = "2017",
-		project_name = "Pythagoras Tree"
+	std::string name = "Azeem Mirza", year = "2017", project_name = "Pythagoras Tree"
 };
+
+*/
+
+std::string copyrights() {
+	std::string s = "\n Pythagoras Tree (c) 2018 \n Azeem Mirza - azeemirza.com \n License: MIT (c) 2017 - 2018";
+	return s;
+}
