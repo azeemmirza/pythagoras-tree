61005162cc1830b8e23e12f9a3cb69dbf4bfde96
Azeem Mirza <contact@azeemirza.com>
2018-06-24 01:23:46 +0500

    config.h updated

--- src/headers/CONFIG.h
+++ src/headers/CONFIG.h
@@ -1,12 +1,12 @@
-#define SIZE 10;
-#define DIRECTION UP;
+#define SIZE	10;
+#define DIRECTION	UP;
 
 
 
-#define WIN_POS_X 100; 
-#define WIN_POS_Y 100;
+#define WIN_POS_X	100; 
+#define WIN_POS_Y	100;
 
-#define WIN_SIZE_WIDTH 1000;
-#define WIN_SIZE_HEIGHT 1000;
+#define WIN_SIZE_WIDTH		1000;
+#define WIN_SIZE_HEIGHT		1000;
 
-#define M_PI       3.14159265358979323846
+#define M_PI	3.14159265358979323846
