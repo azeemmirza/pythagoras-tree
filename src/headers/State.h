bc29f56cc53a762fbd7e96c037ff1b8d1d7a861b
Azeem Mirza <contact@azeemirza.com>
2018-09-01 02:49:54 +0500

    State Class definition

--- src/headers/State.h
+++ src/headers/State.h
@@ -15,26 +15,13 @@ public:
 	float size;
 	Turn turn;
 
+	State();
+	State(float size);
+	State(Point3 CP, float CD, float size);
 
-	State() {
-		this->CP.set(0, 0, 0);
-		this->CD = 0;
-		this->size = SIZE;
-		this->turn = DIRECTION;
-	}
-	State(float size) {
-		this->CP = CP;
-		this->CD = CD;
-		this->size = size;
-		this->turn = DIRECTION;
-	}
-	State(Point3 CP, float CD, float size) {
-		this->size = size;
-	}
+	void setSize(float size);
+	void printState();
 
-	void setSize(float size) {
-		this->size = size;
-	}
 };
 
 #endif // !STATE_H
