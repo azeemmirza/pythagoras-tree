e0327bd316ea61c68e3e1f341739adffa352575a
Azeem Mirza <contact@azeemirza.com>
2018-10-14 12:24:22 +0500

    old Config reset (Only for internal Headers)

--- src/headers/CONFIG.h
+++ src/headers/CONFIG.h
@@ -1,13 +1,5 @@
-#define SIZE	10;
-#define DIRECTION	UP;
-
-#define WIN_TITLE "Pythagoras Tree";
-
-
-#define WIN_POS_X	100; 
-#define WIN_POS_Y	100;
-
-#define WIN_SIZE_WIDTH		1000;
-#define WIN_SIZE_HEIGHT		1000;
-
 #define M_PI	3.14159265358979323846
+
+//Defualt Config
+#define SIZE	30;
+#define DIRECTION	UP;
