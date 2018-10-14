da4cfb6396dde2b57f0019a6fa6a27df752fef82
Azeem Mirza <contact@azeemirza.com>
2018-10-14 12:23:58 +0500

    Custom config header file for tree configration

---
+++ src/config.h
@@ -0,0 +1,27 @@
+#pragma once
+//WINDOWS 
+#define WIN_TITLE "Pythagoras Tree";
+
+#define WIN_POS_X	2000; 
+#define WIN_POS_Y	100;
+
+#define WIN_SIZE_WIDTH		1000;
+#define WIN_SIZE_HEIGHT		900;
+
+
+//RULE_SET_VARIABLE -> RULE_SET_DEFINITION 
+#define RULE_SET_VARIABLE	'S';
+#define RULE_SET_DEFINITION "X [ - M S ] [ + M S ]";
+
+// TREE CONFIGS
+#define AXIOM "S";
+#define SIZE	30;
+#define DIRECTION	UP;
+#define ANGLE 60.0;
+#define ITERATIONS 10;
+
+
+
+
+
+
